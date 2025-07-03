d502357806dbeba9cb47c227d6ea20871c87ed24
LAFlame594 <lilflame694@gmail.com>
2025-07-03 15:22:08 +0300

    Updated contact form and email setup

--- app/views.py
+++ app/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def base_view(request):
+    success = False
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        subject = f"Portofolio Contact from {name}"
+        full_message = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+
+        send_mail(
+            subject,
+            full_message,
+            settings.DEFAULT_FROM_EMAIL, # sender
+            [settings.CONTACT_EMAIL], # recipient
+            fail_silently=False,
+        )
+        success = True
+    
+    return render(request, 'base.html', {'success': success})
--- my_portofolio/settings.py
+++ my_portofolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lilflame694@gmail.com'
+EMAIL_HOST_PASSWORD = 'zril uhot alne rfmu '
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+CONTACT_EMAIL = 'lilflame694@gmail.com'
